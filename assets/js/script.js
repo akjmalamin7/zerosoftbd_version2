14c323cea5b88d77df6d47ba1b2f9472a62e77e9
akjmalamin7 <thpi205526@gmail.com>
2020-09-03 13:16:10 +0600

    complete home page without responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,14 @@
-$(document).ready(function(){
-    //mix it up
-    var mixer = mixitup('.latest-product');
-    var mixer = mixitup(containerEl);
-    var mixer = mixitup(containerEl, {
-        selectors: {
-            target: '.blog-item'
-        },
-        animation: {
-            duration: 300
-        }
-    });
 
-    // client feadback slider
-    
-});
-
-$(document).ready(function(){
-    $('.feadback-slider ').owlCarousel({
+jQuery(document).ready(function(){
+    //Owl carousel
+    jQuery('.feadback-slider ').owlCarousel({
         items:2,
         loop:true,
         margin:10,
         nav:true,
         margin:20,
         autoplay:true,
-        autoplayTimeout:5000,
+        autoplayTimeout:10000,
         dots:false,
         slideTransition:'linear',
         slideBy:1,
@@ -36,6 +21,59 @@ $(document).ready(function(){
                 items:2,
             }
         }
-        
     });
-})
+
+    //partner logo
+    $(".partner-logo").owlCarousel({
+        items:6,
+        margin:30,
+        loop:true,
+        slideBy:1,
+        nav:false,
+        autoplay:true,
+        smartSpeed:1000,
+        autoplayTimeout:4000,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:6
+            }
+        }
+    })
+
+    //mixitup
+    var mixer = mixitup('.latest-product');
+    var mixer = mixitup(containerEl);
+    var mixer = mixitup(containerEl, {
+        selectors: {
+            target: '.blog-item'
+        },
+        animation: {
+            duration: 300
+        }
+    });
+    //Counter up
+    jQuery('.countUp').counterUp({
+        delay: 10,
+        time: 1000
+    });
+    
+  
+   
+    
+});
+
+//sticky header
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 300) {
+        $('nav').addClass('fixed-header');
+    }
+    else {
+        $('nav').removeClass('fixed-header');
+    }
+});
